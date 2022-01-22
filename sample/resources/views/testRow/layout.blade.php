c1bf212306edb508f473fc85d642230bc59656ec
공대여자 <mins01.lycos.co.kr@gmail.com>
2022-01-22 19:17:56 +0900

    Update layout.blade.php

--- sample/resources/views/testRow/layout.blade.php
+++ sample/resources/views/testRow/layout.blade.php
@@ -1,6 +1,8 @@
 {{-- 테이블 테스트 기본 레이아웃 --}}
-@extends($extends??'layouts.default')
-@section('title', $title??'기본 타이틀')
+@sectionMissing('title')
+	@section('title', $title??'기본 타이틀')
+@endif
+@extends($layoutExtends??'layouts.default')
 @section('head')
 	<!-- 추가 head 부분 - layout -->
 @endsection
